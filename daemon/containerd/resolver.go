d2ecfa6c301c1898e244581707bad5e17aee7fa9
Paweł Gronowski <pawel.gronowski@docker.com>
2022-07-21 18:19:00 +0200

    containerd/auth: Check if registry hostname matches
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/containerd/resolver.go
+++ daemon/containerd/resolver.go
@@ -4,13 +4,23 @@ import (
 	"github.com/containerd/containerd/remotes"
 	"github.com/containerd/containerd/remotes/docker"
 	registrytypes "github.com/docker/docker/api/types/registry"
+	"github.com/docker/docker/registry"
+	"github.com/sirupsen/logrus"
 )
 
 func newResolverFromAuthConfig(authConfig *registrytypes.AuthConfig) (remotes.Resolver, docker.StatusTracker) {
 	opts := []docker.RegistryOpt{}
 
 	if authConfig != nil {
-		authorizer := docker.NewDockerAuthorizer(docker.WithAuthCreds(func(_ string) (string, string, error) {
+		cfgHost := registry.ConvertToHostname(authConfig.ServerAddress)
+		if cfgHost == registry.IndexHostname {
+			cfgHost = registry.DefaultRegistryHost
+		}
+		authorizer := docker.NewDockerAuthorizer(docker.WithAuthCreds(func(host string) (string, string, error) {
+			if cfgHost != host {
+				logrus.WithField("host", host).WithField("cfgHost", cfgHost).Warn("Host doesn't match")
+				return "", "", nil
+			}
 			if authConfig.IdentityToken != "" {
 				return "", authConfig.IdentityToken, nil
 			}
