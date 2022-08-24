4b8cc7cf313bda17ccfa3445053cf4f9d2396a88
Sebastiaan van Stijn <github@gone.nl>
2022-08-24 15:27:38 +0200

    image commit: use configured snapshotter instead of default
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/containerd/image_commit.go
+++ daemon/containerd/image_commit.go
@@ -79,7 +79,7 @@ func (i *ImageService) CommitImage(ctx context.Context, cc backend.CommitConfig)
 
 	var (
 		differ = i.client.DiffService()
-		sn     = i.client.SnapshotService(containerd.DefaultSnapshotter)
+		sn     = i.client.SnapshotService(i.snapshotter)
 	)
 
 	// Don't gc me and clean the dirty data after 1 hour!
@@ -105,7 +105,7 @@ func (i *ImageService) CommitImage(ctx context.Context, cc backend.CommitConfig)
 	}
 
 	layers := append(ocimanifest.Layers, diffLayerDesc)
-	commitManifestDesc, configDigest, err := writeContentsForImage(ctx, containerd.DefaultSnapshotter, baseImg, imageConfig, layers)
+	commitManifestDesc, configDigest, err := writeContentsForImage(ctx, i.snapshotter, baseImg, imageConfig, layers)
 	if err != nil {
 		return "", err
 	}
