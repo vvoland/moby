fdbb23a96ad3a192996e8ca18e37a52bafa1bec4
Paweł Gronowski <pawel.gronowski@docker.com>
2022-09-01 17:03:10 +0200

    c8d/resolver: Fallback to http for insecure registries
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/containerd/resolver.go
+++ daemon/containerd/resolver.go
@@ -1,6 +1,9 @@
 package containerd
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/containerd/containerd/remotes"
 	"github.com/containerd/containerd/remotes/docker"
 	registrytypes "github.com/docker/docker/api/types/registry"
@@ -9,10 +12,10 @@ import (
 )
 
 func (i *ImageService) newResolverFromAuthConfig(authConfig *registrytypes.AuthConfig) (remotes.Resolver, docker.StatusTracker) {
-	hostsFn := i.registryHosts.RegistryHosts()
-	hosts := hostsAuthorizerWrapper(hostsFn, authConfig)
-
 	tracker := docker.NewInMemoryTracker()
+	hostsFn := i.registryHosts.RegistryHosts()
+
+	hosts := hostsWrapper(hostsFn, authConfig, i.registryService)
 
 	return docker.NewResolver(docker.ResolverOptions{
 		Hosts:   hosts,
@@ -20,23 +23,29 @@ func (i *ImageService) newResolverFromAuthConfig(authConfig *registrytypes.AuthC
 	}), tracker
 }
 
-func hostsAuthorizerWrapper(hostsFn docker.RegistryHosts, authConfig *registrytypes.AuthConfig) docker.RegistryHosts {
+func hostsWrapper(hostsFn docker.RegistryHosts, authConfig *registrytypes.AuthConfig, regService *registry.Service) docker.RegistryHosts {
 	return docker.RegistryHosts(func(n string) ([]docker.RegistryHost, error) {
 		hosts, err := hostsFn(n)
-		if err == nil {
-			for idx, host := range hosts {
-				if host.Authorizer == nil {
-					var opts []docker.AuthorizerOpt
-					if authConfig != nil {
-						opts = append(opts, authorizationCredsFromAuthConfig(*authConfig))
-					}
-					host.Authorizer = docker.NewDockerAuthorizer(opts...)
-					hosts[idx] = host
-				}
-			}
+		if err != nil {
+			return nil, err
 		}
 
-		return hosts, err
+		for idx, host := range hosts {
+			if host.Authorizer == nil {
+				var opts []docker.AuthorizerOpt
+				if authConfig != nil {
+					opts = append(opts, authorizationCredsFromAuthConfig(*authConfig))
+				}
+				host.Authorizer = docker.NewDockerAuthorizer(opts...)
+
+				isInsecure := regService.IsInsecureRegistry(host.Host)
+				if host.Client.Transport != nil && isInsecure {
+					host.Client.Transport = httpFallback{super: host.Client.Transport}
+				}
+				hosts[idx] = host
+			}
+		}
+		return hosts, nil
 	})
 }
 
@@ -56,4 +65,25 @@ func authorizationCredsFromAuthConfig(authConfig registrytypes.AuthConfig) docke
 		}
 		return authConfig.Username, authConfig.Password, nil
 	})
+}
+
+type httpFallback struct {
+	super http.RoundTripper
+}
+
+func (f httpFallback) RoundTrip(r *http.Request) (*http.Response, error) {
+	resp, err := f.super.RoundTrip(r)
+	if err != nil {
+		if strings.Contains(err.Error(), "http: server gave HTTP response to HTTPS client") {
+			plainHttpUrl := *r.URL
+			plainHttpUrl.Scheme = "http"
+
+			plainHttpRequest := *r
+			plainHttpRequest.URL = &plainHttpUrl
+
+			return http.DefaultTransport.RoundTrip(&plainHttpRequest)
+		}
+	}
+
+	return resp, err
 }
